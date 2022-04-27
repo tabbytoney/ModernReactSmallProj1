b5e0d8a1f1e82bb41babccfde167311594fd5dd5
Tabby Douglass <tabbytoney@gmail.com>
2022-04-27 12:29:02 -0500

    Removing comment jsx from App into its own component

--- src/index.js
+++ src/index.js
@@ -1,28 +1,10 @@
 // Import React and React Dom libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import faker from '@faker-js/faker';
 
 // Create a react component
 const App = () => {
-  return (
-    <div className='ui container comments'>
-      <div className='comment'>
-        <a href='/' className='avatar'>
-          <img alt='avatar' src={faker.image.avatar()} />
-        </a>
-        <div className='content'>
-          <a href='/' className='author'>
-            Bob
-          </a>
-          <div className='metadata'>
-            <span className='date'>Today at 6:00PM</span>
-            <div className='text'>Nice blog post</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div className='ui container comments'></div>;
 };
 
 // Take the react component and show it on the screen
