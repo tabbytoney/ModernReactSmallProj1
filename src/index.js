220d0f306f16eace8660478576463e377d0523ef
Tabby Douglass <tabbytoney@gmail.com>
2022-04-27 11:07:08 -0500

    install semantic-ui styling via cdn

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 // Import React and React Dom libraries
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 // Create a react component
 const App = () => {
-  const buttonText = { text: 'Click Me' };
-  const labelText = 'Enter name: ';
-  return (
-    <div>
-      <label className='label' htmlFor='name'>
-        {labelText}
-      </label>
-      <input id='name' type='text' />
-      <button style={{ backgroundColor: 'blue', color: 'white' }}>{buttonText.text}</button>
-    </div>
-  );
+  return <div>hi</div>;
 };
 
 // Take the react component and show it on the screen
-ReactDOM.render(<App />, document.querySelector('#root'));
+// below is an earlier version of React method
+// ReactDOM.render(<App />, document.querySelector('#root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
